23b6234775edee42e67ea67388d1426a3ad75219
Ebnen <ebenzy1996@gmail.com>
2024-04-30 05:06:41 -0700

    eds

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,10 +9,10 @@
 
 void bubble_sort(int *array, size_t size)
 {
-if (array == NULL || size < 2)
-	return;
 size_t i, j;
 int temp;
+if (array == NULL || size < 2)
+	return;
 for (i = 0; i < size - 1; i++)
 {
 for (j = 0; j < size - 1 - i; j++)
